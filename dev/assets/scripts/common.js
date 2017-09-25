db5e614df47c15910c4bd3aca52e22569af82be2
Vadim Ilchenko <vvadimko@gmail.com>
2017-09-26 00:47:39 +0300

    add files

--- dev/assets/scripts/common.js
+++ dev/assets/scripts/common.js
@@ -1,5 +1,28 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-mobileNav();
+
+	$('.menu__link').click(function(){
+		var link = $(this).attr('href');
+		$('html, body').animate({scrollTop:$(link).position().top}, 1000);
+	});
+
+	$(window).scroll(function(){
+		if ($(window).scrollTop() >= $('.header').height()) {
+			$('.header__nav').hasClass('fixed') ? '' :
+			(
+				$('.header__nav').addClass('fixed'),
+				$('.header__nav').fadeIn(),
+				$(window).width() < 769 ? $('.header__nav').css('display', 'flex') : ''
+				)
+		}
+		else {
+			$('.header__nav').hasClass('fixed') ?
+			(
+				$('.header__nav').removeClass('fixed'),
+				$('.header__nav').css('display', '')
+				) : '';
+		}
+	});
+	mobileNav();
 });
 window.onload = function() {
 
@@ -7,20 +30,16 @@ window.onload = function() {
 //Mobile menu function
 function mobileNav() {
 	var menu 			= document.querySelector('.header__menu'),
-		mobilemenu 		= document.querySelector('.mobile-nav__menu'),
-		JSinit 			= document.querySelector('.js_mobile-nav'),
-		burger 			= document.querySelectorAll('.burger'),
-		activeClass 	= 'open';
+	mobilemenu 		= document.querySelector('.mobile-nav__menu'),
+	JSinit 			= document.querySelector('.js_mobile-nav'),
+	burger 			= document.querySelectorAll('.burger'),
+	activeClass 	= 'open';
 	function toggle() {
 		JSinit.classList.toggle(activeClass);
 	}
 	if (mobilemenu.innerHTML == 0)
 		mobilemenu.innerHTML = menu.innerHTML;
-
-	/* for (var i = 0; i < burger.length; i++) {
-			burger[i].addEventListener('click', toggle);
-		} */
 	for (var i = 0; i < burger.length; i++) {
 		burger[i].onclick = toggle;
-		};
+	};
 }
